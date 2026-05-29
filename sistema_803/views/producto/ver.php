520a2ffa1287f7f1fae7bb9d1889c4ef3a0012e2
226w0536-hash <226w0536@zongolica.tecnm.mx>
2026-05-29 02:46:48 -0600

    Update ver.php

--- sistema_803/views/producto/ver.php
+++ sistema_803/views/producto/ver.php
@@ -14,10 +14,12 @@
         <div class="data">
             <p class="description"><?= $product->descripcion ?></p>
             <p class="price"><?= $product->precio ?>$</p>
+            
             <a href="<?=base_url?>carrito/add?id=<?=$product->id?>" class="button">Comprar</a>
         </div>
         
     </div>
-    <?php else: ?>
+
+<?php else: ?>
     <h1>El producto no existe</h1>
 <?php endif; ?>
