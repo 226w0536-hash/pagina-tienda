c98753d871baa2e6ee21a91273bf1e50b443c274
226w0536-hash <226w0536@zongolica.tecnm.mx>
2026-05-28 22:12:47 -0600

    Enhance autoload to support models and helpers
    Updated autoload function to include models and helpers.

--- sistema_803/autoload.php
+++ sistema_803/autoload.php
@@ -1,7 +1,11 @@
 <?php
-
-function controllers_autoload($classname){
-	include 'controllers/' . $classname . '.php';
+function app_autoload($classname){
+    if (file_exists('controllers/' . $classname . '.php')) {
+        include 'controllers/' . $classname . '.php';
+    } elseif (file_exists('models/' . $classname . '.php')) {
+        include 'models/' . $classname . '.php';
+    } elseif (file_exists('helpers/' . $classname . '.php')) {
+        include 'helpers/' . $classname . '.php';
+    }
 }
-
-spl_autoload_register('controllers_autoload');
+spl_autoload_register('app_autoload');
