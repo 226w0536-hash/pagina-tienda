b915ab6c24e2ace6e53f52b1d6c35e0d92c506c2
226w0536-hash <226w0536@zongolica.tecnm.mx>
2026-05-28 22:27:05 -0600

    Update index.php

--- sistema_803/index.php
+++ sistema_803/index.php
@@ -12,35 +12,35 @@ require_once 'views/layout/header.php';
 require_once 'views/layout/sidebar.php';
 
 function show_error(){
-	$error = new ErrorController();
-	$error->index();
+    $error = new ErrorController();
+    $error->index();
 }
 
+// CORRECCIÓN AQUÍ: Aplicamos ucfirst() para asegurar que la clase empiece con mayúscula
 if(isset($_GET['controller'])){
-	$nombre_controlador = $_GET['controller'].'Controller';
-
+    $nombre_controlador = ucfirst($_GET['controller']) . 'Controller';
 }elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
-	$nombre_controlador = controller_default;
-	
+    $nombre_controlador = controller_default;
 }else{
-	show_error();
-	exit();
+    show_error();
+    exit();
 }
 
-if(class_exists($nombre_controlador)){	
-	$controlador = new $nombre_controlador();
-	
-	if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
-		$action = $_GET['action'];
-		$controlador->$action();
-	}elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
-		$action_default = action_default;
-		$controlador->$action_default();
-	}else{
-		show_error();
-	}
+if(class_exists($nombre_controlador)){    
+    $controlador = new $nombre_controlador();
+    
+    // CORRECCIÓN AQUÍ: También validamos el action de forma más limpia
+    if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
+        $action = $_GET['action'];
+        $controlador->$action();
+    }elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
+        $action_default = action_default;
+        $controlador->$action_default();
+    }else{
+        show_error();
+    }
 }else{
-	show_error();
+    show_error();
 }
 
 require_once 'views/layout/footer.php';
