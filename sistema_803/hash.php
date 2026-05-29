44abfc6fa0e4d12393a8ac0cf64cc585e4d04643
226w0536-hash <226w0536@zongolica.tecnm.mx>
2026-05-29 01:43:08 -0600

    Update hash.php

--- sistema_803/hash.php
+++ sistema_803/hash.php
@@ -1,5 +1,5 @@
 <?php
-$password = "tu_contraseña_aqui"; // Pon aquí la contraseña que quieras para el admin
+$password = "contraseña"; // Pon aquí la contraseña que quieras para el admin
 $hash = password_hash($password, PASSWORD_BCRYPT);
 echo "El hash es: " . $hash;
 ?>
