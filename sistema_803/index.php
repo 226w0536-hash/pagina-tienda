cde1261235d4be2dce9a6df9f0971f551e61637e
226w0536-hash <226w0536@zongolica.tecnm.mx>
2026-05-29 00:19:47 -0600

    Update index.php

--- sistema_803/index.php
+++ sistema_803/index.php
@@ -1,11 +1,9 @@
 <?php
 session_start();
-// En la primera línea de tu index.php
-//var_dump($_GET); 
-//die(); // Esto detendrá la página y te mostrará qué parámetros está recibiendo
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
 require_once 'autoload.php';
 require_once 'config/db.php';
 require_once 'config/parameters.php';
@@ -14,35 +12,34 @@ require_once 'views/layout/header.php';
 require_once 'views/layout/sidebar.php';
 
 function show_error(){
-	$error = new errorController();
-	$error->index();
+    $error = new errorController();
+    $error->index();
 }
 
+// 1. Determinar el controlador
 if(isset($_GET['controller'])){
-	$nombre_controlador = $_GET['controller'].'Controller';
-
-}elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
-	$nombre_controlador = controller_default;
-	
-}else{
-	show_error();
-	exit();
+    $nombre_controlador = $_GET['controller'] . 'Controller';
+} else {
+    $nombre_controlador = controller_default;
 }
 
-if(class_exists($nombre_controlador)){	
-	$controlador = new $nombre_controlador();
-	
-	if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
-		$action = $_GET['action'];
-		$controlador->$action();
-	}elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
-		$action_default = action_default;
-		$controlador->$action_default();
-	}else{
-		show_error();
-	}
-}else{
-	show_error();
+// 2. Ejecución dinámica
+if(class_exists($nombre_controlador)){    
+    $controlador = new $nombre_controlador();
+    
+    // 3. Determinar la acción
+    if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
+        $action = $_GET['action'];
+        $controlador->$action();
+    } elseif(!isset($_GET['controller']) && !isset($_GET['action'])) {
+        // Carga por defecto si no hay nada en la URL
+        $action_default = action_default;
+        $controlador->$action_default();
+    } else {
+        show_error();
+    }
+} else {
+    show_error();
 }
 
 require_once 'views/layout/footer.php';
